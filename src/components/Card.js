8cde3e0b6589d506465b34a0c88afedb6dfff513
marianaCarbonell <marianacarbonell@gmail.com>
2020-03-15 22:09:19 -0300

    cambios

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+
+    render() {
+        return (
+            <h1>   Mar </h1>
+
+        )
+
+    }
+
+}
+
+export default Card;
