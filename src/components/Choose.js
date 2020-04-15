f784fca3a930913b87919fc9067e8855cd467845
marianaCarbonell <marianacarbonell@gmail.com>
2020-04-15 16:10:55 -0400

    grid bootstrap

--- src/components/Choose.js
+++ src/components/Choose.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import logo from '../recursos/logo.png';
+import logo from "../recursos/logo.png";
+import StyleChoose from "../components/StyleChoose.css";
 
 class Choose extends Component {
   render() {
@@ -8,25 +9,31 @@ class Choose extends Component {
         <div className="logo">
           <img src={logo} alt="" />
         </div>
-        <div className="question">
-          <h1>Cuánto tiempo tienes?</h1>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12  question">
+              <h1>Cuánto tiempo tienes? </h1>
+            </div>
+          </div>
         </div>
-        <div className="firstButtons">
-          <button type="button" class="btn btn-outline-warning">
-            30 minutos o menos
-          </button>
-          <button type="button" class="btn btn-outline-warning">
-            30 a 60 minutos
-          </button>
-        </div>
+        <div className="row">
+          <div className="col-lg-6 firstButtons">
+            <button type="button" class="btn btn-outline-warning ">
+              30 minutos o menos
+            </button>
+            <button type="button" class="btn btn-outline-warning">
+              30 a 60 minutos
+            </button>
+          </div>
 
-        <div className="secondButtons">
-          <button type="button" class="btn btn-outline-warning">
-            1 a 2 horas
-          </button>
-          <button type="button" class="btn btn-outline-warning">
-            2 horas o más
-          </button>
+          <div className="col-lg-6 secondButtons">
+            <button type="button" class="btn btn-outline-warning">
+              1 a 2 horas
+            </button>
+            <button type="button" class="btn btn-outline-warning">
+              2 horas o más
+            </button>
+          </div>
         </div>
       </React.Fragment>
     );
