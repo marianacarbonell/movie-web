4a5bb942410906bc6b708435fda8bfca2fc15b51
marianaCarbonell <marianacarbonell@gmail.com>
2020-06-03 16:36:32 -0400

    botones separados y parte del responsive

--- src/components/Choose.js
+++ src/components/Choose.js
@@ -6,35 +6,39 @@ class Choose extends Component {
   render() {
     return (
       <React.Fragment>
+      <div className="container">
+
         <div className="logo">
           <img src={logo} alt="" />
         </div>
         <div className="container">
           <div className="row">
-            <div className="col-sm-12  question">
-              <h1>Cuánto tiempo tienes? </h1>
+            <div className="question">
+              <h1>¿Cuánto tiempo tienes? </h1>
             </div>
           </div>
         </div>
         <div className="row">
-          <div className="col-lg-6 firstButtons">
+          <div className=" firstButton">
             <button type="button" class="btn btn-outline-warning ">
               30 minutos o menos
             </button>
             <button type="button" class="btn btn-outline-warning">
-              30 a 60 minutos
+              1 a 2 horas
             </button>
           </div>
 
-          <div className="col-lg-6 secondButtons">
+          <div className="secondButton">
             <button type="button" class="btn btn-outline-warning">
-              1 a 2 horas
+            30 a 60 minutos
             </button>
             <button type="button" class="btn btn-outline-warning">
               2 horas o más
             </button>
           </div>
         </div>
+
+      </div>
       </React.Fragment>
     );
   }
