969604a251b1d9a47bbd45f29a16e0b7c9a5fed7
marianaCarbonell <marianacarbonell@gmail.com>
2020-06-04 15:57:11 -0400

    botones separados y con ancho completo

--- src/components/Choose.js
+++ src/components/Choose.js
@@ -6,39 +6,37 @@ class Choose extends Component {
   render() {
     return (
       <React.Fragment>
-      <div className="container">
-
-        <div className="logo">
-          <img src={logo} alt="" />
-        </div>
         <div className="container">
+          <div className="logo">
+            <img src={logo} alt="" />
+          </div>
+          <div className="container">
+            <div className="row">
+              <div className="question">
+                <h1>¿Cuánto tiempo tienes? </h1>
+              </div>
+            </div>
+          </div>
           <div className="row">
-            <div className="question">
-              <h1>¿Cuánto tiempo tienes? </h1>
+            <div className=" firstButton">
+              <button type="button" class="btn btn-outline-warning ">
+                30 minutos o menos
+              </button>
+              <button type="button" class="btn btn-outline-warning">
+                1 a 2 horas
+              </button>
+            </div>
+
+            <div className="secondButton">
+              <button type="button" class="btn btn-outline-warning">
+                30 a 60 minutos
+              </button>
+              <button type="button" class="btn btn-outline-warning">
+                2 horas o más
+              </button>
             </div>
           </div>
         </div>
-        <div className="row">
-          <div className=" firstButton">
-            <button type="button" class="btn btn-outline-warning ">
-              30 minutos o menos
-            </button>
-            <button type="button" class="btn btn-outline-warning">
-              1 a 2 horas
-            </button>
-          </div>
-
-          <div className="secondButton">
-            <button type="button" class="btn btn-outline-warning">
-            30 a 60 minutos
-            </button>
-            <button type="button" class="btn btn-outline-warning">
-              2 horas o más
-            </button>
-          </div>
-        </div>
-
-      </div>
       </React.Fragment>
     );
   }
